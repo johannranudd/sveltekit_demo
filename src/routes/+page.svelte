75d8dfb936f5aaef2cdff75307e071b25984ea4a
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-11-15 16:40:29 +0100

    dudeling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1 class="text-red-500 p-2 mx-auto container">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let counter = 0;
+	function increment() {
+		counter++;
+	}
+	function decrement() {
+		if (counter > 0) {
+			counter--;
+		}
+	}
+	let title = '';
+	let titleIsLocked = false;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Counter: {counter}</p>
+<div>
+	<button class="primary-btn" on:click={increment}>increment</button>
+	<button class="primary-btn" on:click={decrement}>decrement</button>
+</div>
+<p>Title: {title}</p>
+<input class="border" type="text" bind:value={title} disabled={titleIsLocked} />
+<button class="primary-btn" on:click={() => (titleIsLocked = true)}>Lock title</button>
+<button class="primary-btn" on:click={() => (titleIsLocked = false)}>unock title</button>
+<p>{titleIsLocked}</p>

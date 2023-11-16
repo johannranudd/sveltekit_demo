f8511af7f2b59f87794c7252f4b2b7c5a09745c6
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-11-16 15:43:37 +0100

    implement (app) and reset @ layouts 2

--- src/lib/components/layout/navbar.svelte
+++ src/lib/components/layout/navbar.svelte
@@ -17,8 +17,8 @@
 	<h3>Logo</h3>
 	<button on:click={changeColorMode}>color mode</button>
 	<ul class="flex gap-4">
-		<li><a href="./">Home</a></li>
-		<li><a href="./about">About</a></li>
-		<li><a href="./courses">Courses</a></li>
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/courses">Courses</a></li>
 	</ul>
 </nav>
